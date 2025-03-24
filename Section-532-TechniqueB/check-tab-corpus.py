3dbd1e82082ef212707d135d4ede4ead2648b9ba
LuciaSanzValdivieso <luciasanzvaldivieso@outlook.com>
2025-03-24 10:44:17 +0100

    Delete Section-532-TechniqueB/check-tab-corpus.py

--- Section-532-TechniqueB/check-tab-corpus.py
+++
@@ -1,8 +0,0 @@
-import codecs
-
-corpus = "tab-separated-parallel-segments-corpus.txt"  # Replace with corpus file
-
-with codecs.open(corpus, "r", encoding="utf-8") as file:
-    for line_num, line in enumerate(file, start=1):
-        if "\t" not in line:
-            print(f"Error in line {line_num} of the corpus: {line.strip()}")
